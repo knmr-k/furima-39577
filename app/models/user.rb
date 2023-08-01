0304b6b36e18efb723994750b7a53b8473d58c5f
knmr-k <kanamr.kz@gmail.com>
2023-08-01 19:29:00 +0900

    未使用のアソシエーションのコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
   
   # パスワードは、半角英数字混合での入力が必須である
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
