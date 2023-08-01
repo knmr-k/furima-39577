b30ddacb174c1204f3fbdb668105add111286627
knmr-k <kanamr.kz@gmail.com>
2023-08-01 19:37:48 +0900

    バリデーションの修正
    ・佐々木などの「々」への対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :nickname, presence: true
   # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須である
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須である
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   # お名前カナ(全角)は、全角（カタカナ）での入力が必須である
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   # お名前カナ(全角)は、全角（カタカナ）での入力が必須である
