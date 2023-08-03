fe1df8bbffe6355dd37f035f088d7c9c379c1dac
knmr-k <kanamr.kz@gmail.com>
2023-08-03 17:19:28 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,32 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   # has_one :order
 
-  validates :name, presence: true
-  validates :price, presence: true
-  validates :info, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_date_id, presence: true
+  # ActiveHash
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  # ActiveStorage
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    # validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :price, numericality: { only_integer: true, in: 300..9999999 }
+    validates :info
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_fee_id
+      validates :prefecture_id
+      validates :shipping_date_id
+    end
+    validates :image
+  end
+
 end
